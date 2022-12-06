4c716cc2f890266ae3ff6b1c78d90d7b0a449510
DeadRush <kanjo494@gmail.com>
2022-12-06 19:22:05 +0100

    checkbox

--- zbroj.js
+++ zbroj.js
@@ -1,6 +1,36 @@
+function checkboxEvent(idLeaveChecked) {
+  //postavi sve checkboxove na false
+  document.getElementById("addCheckbox").checked = false
+  document.getElementById("subtractCheckbox").checked = false
+  document.getElementById("multiplyCheckbox").checked = false
+  document.getElementById("divideCheckbox").checked = false
+  //---------------------
+
+
+
+  //postavi checkbox ciji je id jednak idLeaveChecked
+  console.log(idLeaveChecked)
+  document.getElementById(idLeaveChecked).checked = true;
+  //------------
+
+}
 function zbroj() {
-    var a = parseInt(document.getElementById("prviBroj").value);
-    var b = parseInt(document.getElementById("drugiBroj").value);
-    var c = a + b;
-    document.getElementById("rezultat").innerHTML = c;
+  var a = parseInt(document.getElementById("prviBroj").value);
+  var b = parseInt(document.getElementById("drugiBroj").value);
+  var c;
+  if (document.getElementById("addCheckbox").checked) {
+    c = a + b;
   }
+  else if (document.getElementById("subtractCheckbox").checked) {
+     c = a - b;
+  }
+  else if (document.getElementById("multiplyCheckbox").checked) {
+     c = a * b;
+  }
+  else if (document.getElementById("divideCheckbox").checked) {
+     c = a / b;
+  } 
+  document.getElementById("rezultat").innerHTML = c;
+}
+
+
